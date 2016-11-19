08a22ee772e40f6893b5bfaf90d70bd51ac218ce
TylerRosen <tylermrosen@gmail.com>
2016-11-18 19:17:04 -0500

    psychic game final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,47 +3,58 @@
 //Track # of guesses left
 //Track guesses so far
 
-var letters = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var wins= 0;
-var losses= 0;
-var guessesleft= 10
+	var wins= 0;
+	var losses= 0;
+	var guessesleft= 10;
+	var guesses= [];
 
-var key = event.key
+	var computerchoice = letters[Math.floor(Math.random()*letters.length)];
+	var correctletter = letters[computerchoice]
 
-var computerchoice = letters[Math.random();
-console.log(letters.charAt(0));
 
-// var random= Math.random()
-// console.log(letters.charAt(0))
+	var random= Math.random()
 
-document.onkeyup = function() {
-	computerchoice = letters[Math.random()];
-	console.log(computerchoice);
+	document.onkeyup = function() {
+		var key = event.key;
+		guesses.push(key);
 
-	var userchoice = letters();
-	console.log(userchoice);
+		correctletter = computerchoice[Math.floor(Math.random()*(letters.length))]
+		console.log(computerchoice)
 
-	if (userchoice == computerchoice) {
-		console.log("You win!");
-		wins++
-	} else {
-		guessesleft - 1;
+
+		var userchoice = String.fromCharCode(event.keyCode).
+			toLowerCase();
+		console.log(userchoice);
+
+		if (userchoice == computerchoice) {
+			console.log("You win!");
+			wins++
+			guessesleft = 10
+			guesses = []
+			computerchoice = letters[Math.floor(Math.random()*letters.length)];
+
+		} else {
+			guessesleft--
+
+		}
+
+
+		if (guessesleft==0) {
+			console.log("You lose!");
+			losses++;
+			guessesleft =10
+			guesses = [];
+			computerchoice = letters[Math.floor(Math.random()*(letters.length))]
+		}
+
+		var html = "<p>Guess what letter I'm thinking of</p>" +
+		"<p>Wins: " + wins + "</p>" +
+		"<p>Losses: " + losses + "</p>" +
+		"<p>Guesses Left: " + guessesleft + "</p>" +
+		"<p>Your Guesses So Far: " + guesses + "</p>"
+
+		document.querySelector('#game').innerHTML = html;
 
 	}
-
-	if (guessesleft==0) {
-		console.log("You lose!");
-		losses++;
-	}
-
-	var html = "<p>Your Guesses So Far: " + userchoice + "</p>" +
-	"<p>Wins: " + wins + "</p>" + 
-	"<p>Losses:" + losses + "</p>" +
-	"<p>Guesses Left:" + guessesleft + "</p>"
-
-	document.querySelector('#game').innerHTML = html;
-
-	document.querySelector("#textToShow").innerHTML = word
-
-}
