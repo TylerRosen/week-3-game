2507def6ef825cac31c832541f0b9a841786bf88
Tyler Rosen <tylermrosen@gmail.com>
2017-05-26 14:52:23 -0400

    Centered text

---
+++ hangman.js
@@ -0,0 +1,13 @@
+// Global Variables
+// ------------------------------------------------------------------------
+
+var words = ["cat", "fish", "computer"];
+var wordChosen = "";
+var letters = [];
+var blanks = 0;
+var blanksAndSuccesses = [];
+var wrongGuess = [];
+
+// Game counter
+
+var counter = 0;
